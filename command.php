5979d6c9ee44958f270eecf1025c682de5e09cbc
Maros Dovec <maros@grownapps.io>
2023-02-18 21:09:10 +0100

    update composer

--- command.php
+++ command.php
@@ -7,14 +7,19 @@
  * @version 1.0.0-prealpha.1
  */
 
-if ( defined( 'WP_CLI' ) && WP_CLI ) {
-
-    if ( version_compare( PHP_VERSION, '5.5', '<' ) ) {
-        WP_CLI::error( sprintf( 'This WP-CLI package requires PHP version %s or higher.', '5.5' ) );
+if (defined('WP_CLI') && WP_CLI) {
+    if (version_compare(PHP_VERSION, '5.5', '<')) {
+        WP_CLI::error(sprintf('This WP-CLI package requires PHP version %s or higher.', '5.5'));
     }
 
-    if ( version_compare( WP_CLI_VERSION, '1.5.0', '<' ) ) {
-        WP_CLI::error( sprintf( 'This WP-CLI package requires WP-CLI version %s or higher. Please visit %s', '1.5.0', 'https://wp-cli.org/#updating' ) );
+    if (version_compare(WP_CLI_VERSION, '1.5.0', '<')) {
+        WP_CLI::error(
+            sprintf(
+                'This WP-CLI package requires WP-CLI version %s or higher. Please visit %s',
+                '1.5.0',
+                'https://wp-cli.org/#updating'
+            )
+        );
     }
 
     # api, cli
@@ -45,43 +50,41 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
     require __DIR__ . '/src/Commands/Menu.php';
     require __DIR__ . '/src/Commands/String.php';
 
-    WP_CLI::add_hook( 'before_wp_load', function() {
-
-        WP_CLI::add_wp_hook( 'init', function() {
-
+    WP_CLI::add_hook('before_wp_load', function () {
+        WP_CLI::add_wp_hook('init', function () {
             # make sure polylang_mo post type is always registered
-            if ( ! post_type_exists( 'polylang_mo' ) ) {
-                $labels = array( 'name' => __( 'Strings translations', 'polylang' ) );
-                register_post_type( 'polylang_mo', array( 'labels' => $labels, 'rewrite' => false, 'query_var' => false, '_pll' => true ) );
+            if (!post_type_exists('polylang_mo')) {
+                $labels = array('name' => __('Strings translations', 'polylang'));
+                register_post_type('polylang_mo',
+                    array(
+                        'labels' => $labels,
+                        'rewrite' => false,
+                        'query_var' => false,
+                        '_pll' => true
+                    )
+                );
             }
-
         });
-
     });
 
-    WP_CLI::add_hook( 'before_invoke:pll menu', function() {
-
-            # make sure localized (temporary) nav menu locations are always registered
-            require_once POLYLANG_DIR . '/include/nav-menu.php';
-            $pll_nav_menu = new \PLL_Nav_Menu( \PLL() );
-            $pll_nav_menu->create_nav_menu_locations();
-
+    WP_CLI::add_hook('before_invoke:pll menu', function () {
+        # make sure localized (temporary) nav menu locations are always registered
+        require_once POLYLANG_DIR . '/include/nav-menu.php';
+        $pll_nav_menu = new \PLL_Nav_Menu(\PLL());
+        $pll_nav_menu->create_nav_menu_locations();
     });
 
-    // WP_CLI::add_command( 'pll',        Polylang_CLI\Cli::class );
-
-    WP_CLI::add_command( 'pll api',       Polylang_CLI\Commands\ApiCommand::class );
-    WP_CLI::add_command( 'pll cache',     Polylang_CLI\Commands\CacheCommand::class );
-    WP_CLI::add_command( 'pll doctor',    Polylang_CLI\Commands\DoctorCommand::class );
-    WP_CLI::add_command( 'pll flag',      Polylang_CLI\Commands\FlagCommand::class );
-    WP_CLI::add_command( 'pll lang',      Polylang_CLI\Commands\LangCommand::class );
-    WP_CLI::add_command( 'pll menu',      Polylang_CLI\Commands\MenuCommand::class );
-    WP_CLI::add_command( 'pll option',    Polylang_CLI\Commands\OptionCommand::class );
-    WP_CLI::add_command( 'pll plugin',    Polylang_CLI\Commands\PluginCommand::class );
-    WP_CLI::add_command( 'pll post',      Polylang_CLI\Commands\PostCommand::class );
-    WP_CLI::add_command( 'pll post-type', Polylang_CLI\Commands\PostTypeCommand::class );
-    WP_CLI::add_command( 'pll string',    Polylang_CLI\Commands\StringCommand::class );
-    WP_CLI::add_command( 'pll taxonomy',  Polylang_CLI\Commands\TaxonomyCommand::class );
-    WP_CLI::add_command( 'pll term',      Polylang_CLI\Commands\TermCommand::class );
-
+    WP_CLI::add_command('pll api', Polylang_CLI\Commands\ApiCommand::class);
+    WP_CLI::add_command('pll cache', Polylang_CLI\Commands\CacheCommand::class);
+    WP_CLI::add_command('pll doctor', Polylang_CLI\Commands\DoctorCommand::class);
+    WP_CLI::add_command('pll flag', Polylang_CLI\Commands\FlagCommand::class);
+    WP_CLI::add_command('pll lang', Polylang_CLI\Commands\LangCommand::class);
+    WP_CLI::add_command('pll menu', Polylang_CLI\Commands\MenuCommand::class);
+    WP_CLI::add_command('pll option', Polylang_CLI\Commands\OptionCommand::class);
+    WP_CLI::add_command('pll plugin', Polylang_CLI\Commands\PluginCommand::class);
+    WP_CLI::add_command('pll post', Polylang_CLI\Commands\PostCommand::class);
+    WP_CLI::add_command('pll post-type', Polylang_CLI\Commands\PostTypeCommand::class);
+    WP_CLI::add_command('pll string', Polylang_CLI\Commands\StringCommand::class);
+    WP_CLI::add_command('pll taxonomy', Polylang_CLI\Commands\TaxonomyCommand::class);
+    WP_CLI::add_command('pll term', Polylang_CLI\Commands\TermCommand::class);
 }
